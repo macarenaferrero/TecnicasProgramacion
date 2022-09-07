53726bd749be09d9a1f48cf4f80c0362e3d45498
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2022-09-07 20:48:37 -0300

    ok

--- problema1.py
+++ problema1.py
@@ -4,7 +4,7 @@
 lado = input("Ingrese lado del cuadrado:")
 perimetro = int(lado) * 4
 
-print("El perimetro de un cuadrado de " + str(lado) + " es " + (perimetro))
+print("El perimetro de un cuadrado de " + str(lado) + " es " + str(perimetro))
 
 #Con la coma concatenamos y no tenemos que castear.
 #print("El perimetro de un cuadrado de ", lado," es ", perimetro)
